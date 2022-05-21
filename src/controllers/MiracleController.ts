dd9eefefd64f605f050a997e83e991fb8f832200
dingding-21 <phj8221@naver.com>
2022-05-22 07:51:14 +0900

    [FIX] Error Handling

--- src/controllers/MiracleController.ts
+++ src/controllers/MiracleController.ts
@@ -12,29 +12,41 @@ import { MiracleService } from '../services';
  * @access Public
  */
 const getMiracle = async (req: Request, res: Response) => {
-    const error = validationResult(req);
-    if(!error.isEmpty()){
-        console.log(error);
-        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    console.log(error);
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+  }
+
+  const { user_id, _date } = req.params;
+  console.log(_date);
+
+  try {
+    const data = await MiracleService.getMiracle(user_id, _date);
+    if (!data) {
+      return res
+        .status(statusCode.NOT_FOUND)
+        .json(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
     }
-
-    const {user_id,_date} = req.params;
-    console.log(_date);
-
-    
-    try{
-        const data = await MiracleService.getMiracle(user_id, _date);
-        if(data==null){
-            res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND)); 
-        }
-        res.status(statusCode.OK).send(util.success(statusCode.OK,message.READ_TODAY_MIRACLE_SUCCESS,data));
-
-    }catch(error){
-        console.log(error);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR)); 
-   }
-    
-}
+    res
+      .status(statusCode.OK)
+      .send(
+        util.success(statusCode.OK, message.READ_TODAY_MIRACLE_SUCCESS, data)
+      );
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .json(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
 
 /**
  *  @route POST /miracle
